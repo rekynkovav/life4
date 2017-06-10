51f8d924050d3067e84670aae6b45e9d62548b2d
Rekynkovav <Rekynkovav@yandex.ru>
2017-06-11 01:57:08 +0300

    5:16

--- src/Display.java
+++
@@ -1,8 +0,0 @@
-/**
- * Created by Macbook on 29.05.17.
- */
-public abstract class Display {
-
-    private static boolean created = false; // проверка созданно окно или нет!
-
-}
--- src/sample/Main.java
+++ src/sample/Main.java
@@ -1,19 +1,18 @@
 package sample;
 
+import javafx.animation.AnimationTimer;
 import javafx.application.Application;
 import javafx.scene.Group;
 import javafx.scene.Scene;
-import javafx.scene.layout.FlowPane;
 import javafx.scene.paint.Color;
-import javafx.scene.shape.Rectangle;
+import javafx.scene.shape.Line;
 import javafx.stage.Stage;
 import sample.game.Game;
 
 public class Main extends Application {
 
     private static Stage stage; // само окно
-    private static Scene scene; // сцены отоброжаемые внутри окна их можеть быть много и использовать их можно для разных задачь
-    private static int x,y;
+    private static Scene scene,scene1; // сцены отоброжаемые внутри окна их можеть быть много и использовать их можно для разных задачь
     private static Group pane;// на самом пане мы рисуем
     @Override
     public void start(Stage stage) throws Exception {
@@ -22,21 +21,27 @@ public class Main extends Application {
         pane = new Group();
 
         scene = new Scene(pane, 800, 800, Color.BLACK);
-        Game.start();
 
         stage.setTitle("Life");
         stage.setScene(scene);
         stage.show();
+
+        AnimationTimer timer = new AnimationTimer() {
+            @Override
+            public void handle(long now) {
+                Game.start();
+            }
+        };
+        timer.start();
     }
 
     public static void printRectangle(int x, int y){
-        Rectangle rectangle = new Rectangle(x, y, 0, 0);
-        rectangle.setStroke(Color.GREEN);
-        pane.getChildren().add(rectangle);
+        Line line = new Line(x,y,x,y);
+        line.setStroke(Color.FORESTGREEN);
+        pane.getChildren().add(line);
     }
 
-    public static void clearDysplay(){
-
+    public static void clearDisplay(){
 
     }
 
